4e9b374c4cac3935fa27117405f2472e4f3dd155
ztt1375 <2638004277@qq.com>
2018-11-06 19:36:51 +0800

    博客项目

---
+++ server/config/index.js
@@ -0,0 +1,17 @@
+export const db = {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: '654321',
+    multipleStatements: true
+}
+
+export const dbName = {
+    database: 'blog_db'
+}
+
+export const port = 3000
+
+export const baseApi = 'api/v1'
+
+export const secret = 'ashen-one'
